f6b077667997e106d7364a64be7cd0dfd2e386d8
RicardoMartinez.Denver@gmail.com <RicardoMartinez.Denver@gmail.com>
2022-09-08 07:08:52 -0600

    add error functionality to all input fields

--- script.js
+++ script.js
@@ -2,18 +2,48 @@ let onSubmit = () => {
     const form = document.getElementById("form");
 
 
-    const firstName = document.getElementById("first-name").value;
-    const lastName = document.getElementById("last-name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    const fnElement = document.getElementById("first-name");
+    const firstName = fnElement.value;
+
+    const lnElement = document.getElementById("last-name");
+    const lastName = lnElement.value;
+
+    const emElement = document.getElementById("email");
+    const email = emElement.value;
+
+    const psElement = document.getElementById("password")
+    const password = psElement.value;
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
         if (firstName === '' || firstName === null) {
-            element = document.getElementById("first-name");
-            element.classList.remove("input-margin");
-            document.getElementById("name-error").innerHTML = "First Name cannot be empty";
+            document.getElementById("fname-error").innerHTML = "First Name cannot be empty";
+            fnElement.classList.add("error-border");
+        }
+
+        if (lastName === '' || lastName === null) {
+            document.getElementById("lname-error").innerHTML = "Last Name cannot be empty";
+            lnElement.classList.add("error-border");
+        }
+
+        if (password === '' || password === null) {
+            document.getElementById("password-error").innerHTML = "Password cannot be empty";
+            psElement.classList.add("error-border");
+        }
+
+        if (!validateEmail(email)) {
+            document.getElementById("email-error").innerHTML = "Looks like this is not an email";
+            emElement.classList.add("error-border");
         }
     })
+}
+
+let validateEmail = (input) => {
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (input.match(mailFormat))
+        return true;
+    else
+        return false;
 }
